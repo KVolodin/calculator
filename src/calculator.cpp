55e33064bb58b341c13d1eb43ab52c3631e50be5
Kirill <lagons@mail.ru>
2022-03-11 23:56:23 +0300

    Convert tab to space

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -9,20 +9,20 @@
 
 int main()
 {
-	char *locale = setlocale( LC_ALL, "" );
-	auto rpn = std::make_unique<ReversePolishNotation>();
-	std::string input;
-	while ( true )
-	{
-		std::cout << "Введите выражение:" << std::endl;
-		std::getline( std::cin, input );
-		auto result = 0.;
-		auto succes = rpn->calc( input, result );
-		if ( !succes )
-			std::cout << rpn->getOut() << std::endl;
-		else
-			std::cout << "Результат выражения: " << std::fixed << std::setprecision( 2 ) << result << std::endl;
+    char *locale = setlocale( LC_ALL, "" );
+    auto rpn = std::make_unique<ReversePolishNotation>();
+    std::string input;
+    while ( true )
+    {
+        std::cout << "Введите выражение:" << std::endl;
+        std::getline( std::cin, input );
+        auto result = 0.;
+        auto succes = rpn->calc( input, result );
+        if ( !succes )
+            std::cout << rpn->getOut() << std::endl;
+        else
+            std::cout << "Результат выражения: " << std::fixed << std::setprecision( 2 ) << result << std::endl;
 
-		std::cout << std::endl;
-	}
+        std::cout << std::endl;
+    }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -4,74 +4,74 @@
 class CalculatorTest : public ::testing::Test
 {
 protected:
-	void SetUp() override
-	{
-		rpn = std::make_unique<ReversePolishNotation>();
-		result = 0.;
-	}
-	std::unique_ptr<ReversePolishNotation> rpn;
-	double result;
-	static constexpr auto ACCURACY = 0.5;
+    void SetUp() override
+    {
+        rpn = std::make_unique<ReversePolishNotation>();
+        result = 0.;
+    }
+    std::unique_ptr<ReversePolishNotation> rpn;
+    double result;
+    static constexpr auto ACCURACY = 0.5;
 };
 
 TEST_F( CalculatorTest, TestFromTask )
 {
-	ASSERT_TRUE( rpn->calc( "-1 + 5 - 3", result ) );
-	ASSERT_NEAR( 1, result, ACCURACY );
-	
-	ASSERT_TRUE( rpn->calc( "-10 + (8 * 2.5) - (3 / 1,5)", result ) );
-	ASSERT_NEAR( 8, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "-1 + 5 - 3", result ) );
+    ASSERT_NEAR( 1, result, ACCURACY );
+    
+    ASSERT_TRUE( rpn->calc( "-10 + (8 * 2.5) - (3 / 1,5)", result ) );
+    ASSERT_NEAR( 8, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( "1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5)", result ) );
-	ASSERT_NEAR( 11, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5)", result ) );
+    ASSERT_NEAR( 11, result, ACCURACY );
 
-	ASSERT_FALSE( rpn->calc( "1.1 + 2.1 + abc", result ) );
+    ASSERT_FALSE( rpn->calc( "1.1 + 2.1 + abc", result ) );
 }
 
 TEST_F( CalculatorTest, TestSpace )
 {
-	ASSERT_TRUE( rpn->calc( "                                    -1 +                           80", result ) );
-	ASSERT_NEAR( 79, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "                                    -1 +                           80", result ) );
+    ASSERT_NEAR( 79, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( "      -8                                   +                           80 + 1.5 - 2.5", result ) );
-	ASSERT_NEAR( 71, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "      -8                                   +                           80 + 1.5 - 2.5", result ) );
+    ASSERT_NEAR( 71, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( "(      -200 + 20000    ) - (    6000 + 100000    )", result ) );
-	ASSERT_NEAR( -86200, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "(      -200 + 20000    ) - (    6000 + 100000    )", result ) );
+    ASSERT_NEAR( -86200, result, ACCURACY );
 
 }
 
 TEST_F( CalculatorTest, TestDelimeter )
 {
-	ASSERT_TRUE( rpn->calc( "12,4 - 12.4 + 10.3 - 10,3", result ) );
-	ASSERT_NEAR( 0, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "12,4 - 12.4 + 10.3 - 10,3", result ) );
+    ASSERT_NEAR( 0, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( "1 + 2,5 - 1.5 + 15.3 - 12,3", result ) );
-	ASSERT_NEAR( 5, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( "1 + 2,5 - 1.5 + 15.3 - 12,3", result ) );
+    ASSERT_NEAR( 5, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( " -3 / 1,5 * 1.5 / 12", result ) );
-	ASSERT_NEAR( -0.25, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( " -3 / 1,5 * 1.5 / 12", result ) );
+    ASSERT_NEAR( -0.25, result, ACCURACY );
 }
 
 TEST_F( CalculatorTest, TestBracket )
 {
-	ASSERT_TRUE( rpn->calc( " ( -8 ) + ( 8 ) ", result ) );
-	ASSERT_NEAR( 0, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( " ( -8 ) + ( 8 ) ", result ) );
+    ASSERT_NEAR( 0, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( " ( (((((((((((((((-8 ))))))))))))))) ) + ( 8 ) ", result ) );
-	ASSERT_NEAR( 0, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( " ( (((((((((((((((-8 ))))))))))))))) ) + ( 8 ) ", result ) );
+    ASSERT_NEAR( 0, result, ACCURACY );
 
-	ASSERT_TRUE( rpn->calc( " ( (((((((((((((((-5 + 3 ) - 20) + 12) -12) *3) /2) *100) -111 + 12) + 201.5) - 211.1) + 10) -1123) + 200000) / 2) + 23) -0)", result ) );
-	ASSERT_NEAR( 97762.2, result, ACCURACY );
+    ASSERT_TRUE( rpn->calc( " ( (((((((((((((((-5 + 3 ) - 20) + 12) -12) *3) /2) *100) -111 + 12) + 201.5) - 211.1) + 10) -1123) + 200000) / 2) + 23) -0)", result ) );
+    ASSERT_NEAR( 97762.2, result, ACCURACY );
 }
 
 TEST_F( CalculatorTest, DivZero )
 {
-	ASSERT_FALSE( rpn->calc( " ( -8 ) + ( 8 ) * 10 / 0 ", result ) );
+    ASSERT_FALSE( rpn->calc( " ( -8 ) + ( 8 ) * 10 / 0 ", result ) );
 
-	ASSERT_FALSE( rpn->calc( " 0 / 0 ", result ) );
+    ASSERT_FALSE( rpn->calc( " 0 / 0 ", result ) );
 
-	ASSERT_FALSE( rpn->calc( " ( (((((((((((((((-5 + 3 ) - 20) + 12) -12) *3) /2) *100) -111 + 12) + 201.5) - 211.1) + 10) -1123) + 200000) / 2) + 23) -0) / 0", result ) );
+    ASSERT_FALSE( rpn->calc( " ( (((((((((((((((-5 + 3 ) - 20) + 12) -12) *3) /2) *100) -111 + 12) + 201.5) - 211.1) + 10) -1123) + 200000) / 2) + 23) -0) / 0", result ) );
 }
 
 int main(int argc, char **argv) {
